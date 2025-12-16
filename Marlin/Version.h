d604fb8fed9c4ba133fbe3a943aea374c694bb9e
Aditya Upadhyay <adityau930@gmail.com>
2025-12-16 14:48:25 -0500

    version rollback

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-//#define SHORT_BUILD_VERSION "Cool Project"
+//#define SHORT_BUILD_VERSION "bugfix-2.1.x"
 
 /**
  * Verbose version identifier which should contain a reference to the location
--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "Cool Project"
+  #define SHORT_BUILD_VERSION "bugfix-2.1.x"
 #endif
 
 /**
